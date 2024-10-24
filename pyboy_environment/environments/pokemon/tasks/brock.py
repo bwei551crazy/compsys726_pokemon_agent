c6ceb057253382591ed719261f2cf89443365b5b
bwei551crazy <crazykwaibao@gmail.com>
2024-10-24 17:48:36 +1300

    Backup #3

--- pyboy_environment/environments/pokemon/tasks/brock.py
+++ pyboy_environment/environments/pokemon/tasks/brock.py
@@ -47,18 +47,16 @@ class PokemonBrock(PokemonEnvironment):
             headless=headless,
         )
     
-        self.prev_xy = {()}
-        self.first_goal = False
-        self.reached_first = 0
-        self.stuck_counter = 0
-        self.num_battle = 0
-        self.before_prior_count = 0
-        self.location = {"OAKS_LAB,"}
-        self.location_counter = 0 #Num of times it stays within the same location
-        self.inBattle = False
-        self.prev_enemy_hp = 0
-        self.button_chosed  = 0
-    
+        self.same_loc = 0
+        self.turn_unreward = 0 
+        self.y_reward = 0
+        self.prev_turn = -1
+        self.prev_loc = {""}
+        self.in_battle = True
+        self.different_loc = False
+        self.complete_stuck = 0
+
+
     def _run_action_on_emulator(self, action_array: np.ndarray) -> None:
         action = action_array[0]
         action = min(action, 0.99)
@@ -67,8 +65,6 @@ class PokemonBrock(PokemonEnvironment):
         # We need to convert this to an action that the emulator can understand
         bins = np.linspace(0, 1, len(self.valid_actions) + 1)
         button = np.digitize(action, bins) - 1
-
-        self.button_chosed = button
 
         # Push the button for a few frames
         self.pyboy.send_input(self.valid_actions[button])
@@ -81,116 +77,88 @@ class PokemonBrock(PokemonEnvironment):
 
     def _movement_reward(self, new_state: dict[str, any]) -> int:
         reward = 0
-        if self.inBattle:
+
+        if self.in_battle:
             return 0
         else:
-            # counter_x = 0
-            # counter_y = 0
-            if ((new_state["location"]["x"], new_state["location"]["y"]) != (self.prior_game_stats["location"]["x"], self.prior_game_stats["location"]["y"])):
+            if ((new_state["location"]["y"]) < (self.prior_game_stats["location"]["y"])) and (new_state["location"]["map"] != "OAKS_LAB,"):
+                #may add additional rewards for reaching hear y = 0
+                if self.y_reward == 2:
+                    self.y_reward = 0 #attempt to make it to top by 100 steps
+                if 2 <((new_state["location"]["y"]) - 11) < 5:
+                    reward += 3
+                elif ((new_state["location"]["y"]) - 11) <= 2:
+                    reward += 4
+                reward += 1 + min(self.y_reward, 1.5)
+                self.y_reward += 0.1
 
-                if len(self.prev_xy) == 0:
-                    self.prev_xy.add((self.prior_game_stats["location"]["x"],self.prior_game_stats["location"]["x"]))
-                elif (new_state["location"]["x"], new_state["location"]["y"]) in self.prev_xy:
-                    reward += -1
-                else:
-                    self.prev_xy.add((self.prior_game_stats["location"]["x"],self.prior_game_stats["location"]["x"]))
-                    reward += 1
-                
-                # for i in self.prev_x:
-                #     if i == new_state["location"]["x"]:
-                #         counter_x += 1
-                # for j in self.prev_y:
-                #     if j == new_state["location"]["y"]:
-                #         counter_y += 1
-
-                # if (counter_x > 2) or (counter_y > 2):
-                #     reward -= 1#4#counter_y + counter_x
-                
-                if new_state["location"]["map"] == "PALLET_TOWN,":
-                    if (new_state["location"]["x"], new_state["location"]["y"]) == (11,0):
-                        #if self.first_goal == False: #and self.reached_first < 10:
-                            #self.reached_first += 1
-                        reward += 5#5
-                        # else:
-                        #     reward += 2
-                    elif new_state["location"]["x"] in [10, 12] and new_state["location"]["y"] in [0, 1]:
-                        #if self.first_goal == False: #and self.reached_first < 10:
-                            #self.reached_first += 1
-                        reward +=2 #2
-                        # else:
-                        #     reward += 1
-                elif new_state["location"]["map"] == "ROUTE_1,":
-                    reward +=8#10
-                
-                self.before_prior_count += 1
-                reward += 1 
+            if [(new_state["location"]["x"], new_state["location"]["y"]) != (self.prior_game_stats["location"]["x"], self.prior_game_stats["location"]["y"])]:
+                if self._grass_reward(new_state) :
+                    reward += 2
+                reward += 1
             else:
-                self.stuck_counter += 1
-                if self.stuck_counter > 5:
-                    reward -= min(20, self.stuck_counter) #slowly increases from 5 to 20
-                          
-            return reward
+                self.complete_stuck += 1
+                reward = 0
+                
+            
+            return reward                  
     
     def _location_reward(self, new_state: dict[str, any]) -> float :
         reward = 0
-        if self.inBattle:
+        if self.in_battle:
             return 0
         else:
-            #new location, haven't visited before
-            if new_state["location"]["map"] not in self.location and (new_state["location"]["map"] != self.prior_game_stats["location"]["map"]) :
-                self.location.add(new_state["location"]["map"])
-                reward += 20 #100
-            #New location, but visited before
-            elif (new_state["location"]["map"] != self.prior_game_stats["location"]["map"]) and new_state["location"]["map"] in self.location:
-                if new_state["location"]["map"] == "ROUTE_1,":
-                    reward += 3#self._grass_reward(new_state)*6 #the grass reward always return 1
-                else:
-                    reward += 1#1
-            #SAme location and has visited before
-            elif (new_state["location"]["map"] == self.prior_game_stats["location"]["map"]) and new_state["location"]["map"] in self.location:
-                if new_state["location"]["map"] != "Pallet_Town," or new_state["location"]["map"] != "Route_1,":
-                    reward -= 1#4
-                self.location_counter +=1 #for number of times stuck in the same map
-                #if stuck in route 1, reward enough that the taking reward part isn't that affected
-                if new_state["location"]["map"] == "ROUTE_1,":
-                    reward += 3#self._grass_reward(new_state)*6 #the grass reward always return 1
-                if self.location_counter > 10:
-                    reward -= 1#4
-        
+                
+            if new_state["location"]["map"] not in self.prev_loc:
+                self.prev_loc.add(new_state["location"]["map"])
+                self.different_loc = False
+                reward += 400
+            elif new_state["location"]["map"] != self.prior_game_stats["location"]["map"] and self.different_loc == True:
+                self.different_loc = False #After going into new location that is already visited, cannot come and get this reward until 200 steps has passed
+                reward += 100
+            elif new_state["location"]["map"] == self.prior_game_stats["location"]["map"]:
+                reward += 50 - min(self.same_loc, 50)            #may need to change this from += to -= and - to +
+                self.same_loc += 1                  
+                #after being stuck for 200 ticks
+                if self.same_loc == 2:
+                    self.different_loc = True
+                    reward -= 1
+                    self.same_loc = 0
+            else:
+                reward =0
             return reward
     
     
-    def _in_battle_reward(self, new_state: dict[str, any]) -> float :
+    def _in_battle_reward(self) -> float :
         reward = 0
         if self._read_m(0xD057) != 0:
-            self.inBattle = True
-            #self.num_battle += 1
             enemy_curr_hp = self._read_hp(0xCFE6) #self._read_m(0xCFE6) | self._read_m(0xCFE7)
             enemy_max_hp = self._read_hp(0xCFF4)  #self._read_m(0xCFF4) | self._read_m(0xCFF5)
-            self.prev_enemy_hp = enemy_max_hp
-            
-            #for very first battle
-            if self.button_chosed == 4 and self.num_battle == 0:
-                reward += 20#75
-            elif self.button_chosed == 4 and (self.num_battle > 0):
-                reward += 10#50
-            else:
-                reward -= 1#1
-            
-            if enemy_curr_hp > self.prev_enemy_hp and enemy_curr_hp > 0:
-                self.prev_enemy_hp = enemy_curr_hp
-                reward += 10 #75  # Reward for dealing damage
-            elif enemy_curr_hp == 0:
-                self.inBattle = False
-                self.num_battle += 1
-                reward += 30#100  # Defeated enemy
-            elif enemy_curr_hp <= self.prev_enemy_hp:
-                reward += -1#-5  # No change in enemy HP (reduced penalty)
+            turn_num = self._read_m(0xCCD5) #turn number starts from 0
 
-            return reward
+            if ((enemy_max_hp/2) < enemy_curr_hp < (enemy_max_hp)) and turn_num != self.prev_turn:
+                reward += 400
+                self.prev_turn = turn_num
+            elif (0 < enemy_curr_hp < (enemy_max_hp/2)) and turn_num != self.prev_turn:
+                reward += 200
+                self.prev_turn = turn_num
+            elif (enemy_curr_hp == 0) :
+                reward += 500
+                self.in_battle = False
+            elif turn_num == self.prev_turn:
+                reward -= 0 + min(self.turn_unreward, 1)
+                #when it is continously stuck on the same turn
+                if turn_num < self.prev_turn:
+                    self.turn_unreward += 0.01
+                if self.turn_unreward == 2:
+                    self.turn_unreward = 0
+                self.prev_turn = turn_num
+            self.in_battle = True
         else:
-            self.inBattle = False
-            return reward
+            self.in_battle = False
+            reward = 0
+
+        return reward
 
 
     def _get_state(self) -> np.ndarray:
@@ -219,31 +187,21 @@ class PokemonBrock(PokemonEnvironment):
 
     def _calculate_reward(self, new_state: dict) -> float:
         # Implement your reward calculation logic here
-
-        seen_reward = self._seen_reward(new_state)
         
-        battle_reward = self._in_battle_reward(new_state)
+        location_reward = self._location_reward(new_state)
+        movement_reward = self._movement_reward(new_state)
+        seen_reward = self._seen_reward(new_state)
+        battle_reward = self._in_battle_reward()
         xp_reward = self._xp_reward(new_state)
-
 
         caught_reward = self._caught_reward(new_state)
 
-        if new_state["caught_pokemon"] == 1:
-            caught_reward =caught_reward * 30
-        elif new_state["caught_pokemon"] == 2:
-            caught_reward= caught_reward * 35
-        elif new_state["caught_pokemon"] == 3:
-            caught_reward= caught_reward * 20
-        elif new_state["caught_pokemon"] >= 4:
-            caught_reward= caught_reward * 10
-
-
-        total_reward = (self._movement_reward(new_state) * 0.4
-                        + self._location_reward(new_state) * 0.7
-                        + seen_reward *0.5
-                        + caught_reward *0.8
-                        + battle_reward * 1.0
-                        + xp_reward*0.8)
+        total_reward = (movement_reward * 0.5
+                        + location_reward * 0.8
+                        + seen_reward *0.4
+                        + caught_reward *0.75
+                        + battle_reward * 0.9
+                        + xp_reward*0.5)
 
         return total_reward
 
@@ -254,20 +212,24 @@ class PokemonBrock(PokemonEnvironment):
     def _check_if_truncated(self, game_stats: dict) -> bool:
         # Implement your truncation check logic here
         if self.steps >= 1000:
-            self.prev_xy = {()}
-            self.first_goal = False
-            self.reached_first = 0
-            self.stuck_counter = 0
-            self.num_battle = 0
-            self.before_prior_count = 0
-            self.location = {"OAKS_LAB,"}
-            self.location_counter = 0 #Num of times it stays within the same location
-            self.inBattle = False
-            self.prev_enemy_hp = 0
-            self.button_chosed  = 0
+            self.same_loc = 0
+            self.turn_unreward = 0 
+            self.y_reward = 0
+            self.prev_turn = -1
+            self.prev_loc = {""}
+            self.in_battle = True
+            self.different_loc = False
+            self.complete_stuck = 0
+            return 1
+        elif self.complete_stuck >= 100:
+            self.same_loc = 0
+            self.turn_unreward = 0 
+            self.y_reward = 0
+            self.prev_turn = -1
+            self.prev_loc = {""}
+            self.in_battle = True
+            self.different_loc = False
+            self.complete_stuck = 0
             return 1
         else:
             return 0
-        
-        # Maybe if we run out of pokeballs...? or a max step count
-        #return self.steps >= 1000
