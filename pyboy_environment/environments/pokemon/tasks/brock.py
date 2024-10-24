3c1a0d0dd6ef649239a875f28b488e606798d20d
bwei551crazy <crazykwaibao@gmail.com>
2024-10-24 21:42:58 +1300

    Somewut working
    in one training loop killed 6 pokemon

--- pyboy_environment/environments/pokemon/tasks/brock.py
+++ pyboy_environment/environments/pokemon/tasks/brock.py
@@ -51,11 +51,28 @@ class PokemonBrock(PokemonEnvironment):
         self.turn_unreward = 0 
         self.y_reward = 0
         self.prev_turn = -1
-        self.prev_loc = {""}
-        self.in_battle = True
-        self.different_loc = False
-        self.complete_stuck = 0
+        self.prev_loc = set()
+        self.prev_spot = set()
+        self.no_move = 0
 
+    def reset(self) -> np.ndarray:
+        self.steps = 0
+
+        with open(self.init_path, "rb") as f:
+            self.pyboy.load_state(f)
+
+        self.prior_game_stats = self._generate_game_stats()
+
+        self.same_loc = 0
+        self.turn_unreward = 0 
+        self.y_reward = 0
+        self.prev_turn = -1
+        self.prev_loc = set()
+        self.prev_spot = set()
+        self.no_move = 0
+
+
+        return self._get_state()
 
     def _run_action_on_emulator(self, action_array: np.ndarray) -> None:
         action = action_array[0]
@@ -78,56 +95,70 @@ class PokemonBrock(PokemonEnvironment):
     def _movement_reward(self, new_state: dict[str, any]) -> int:
         reward = 0
 
-        if self.in_battle:
-            return 0
-        else:
-            if ((new_state["location"]["y"]) < (self.prior_game_stats["location"]["y"])) and (new_state["location"]["map"] != "OAKS_LAB,"):
-                #may add additional rewards for reaching hear y = 0
-                if self.y_reward == 2:
-                    self.y_reward = 0 #attempt to make it to top by 100 steps
-                if 2 <((new_state["location"]["y"]) - 11) < 5:
-                    reward += 3
-                elif ((new_state["location"]["y"]) - 11) <= 2:
-                    reward += 4
-                reward += 1 + min(self.y_reward, 1.5)
-                self.y_reward += 0.1
-
-            if [(new_state["location"]["x"], new_state["location"]["y"]) != (self.prior_game_stats["location"]["x"], self.prior_game_stats["location"]["y"])]:
-                if self._grass_reward(new_state) :
+        if (new_state["location"]["x"], new_state["location"]["y"]) != (self.prior_game_stats["location"]["x"], self.prior_game_stats["location"]["y"]):
+            #new coordinate unlocked
+            if (new_state["location"]["x"], new_state["location"]["y"]) not in self.prev_spot:
+                self.prev_spot.add((new_state["location"]["x"], new_state["location"]["y"]))
+                if new_state["location"]["map"] == "PALLET_TOWN,":
                     reward += 2
-                reward += 1
+                elif new_state["location"]["map"] == "ROUTE_1,":
+                    reward += 3
+                elif new_state["location"]["map"] == "VIRIDEAN_CITY,":
+                    reward += 4
+                elif new_state["location"]["map"] == "REDS_HOUSE_1F" or new_state["location"]["map"] == "REDS_HOUSE_2F" or new_state["location"]["map"] == "BLUES_HOUSE":
+                    reward -= 1
             else:
-                self.complete_stuck += 1
-                reward = 0
-                
-            
-            return reward                  
+                if new_state["location"]["map"] == "OAKS_LAB,":
+                    reward -= 1
+
+            if self._grass_reward(new_state) :
+                reward += 1
+            reward += 1
+            #print("Has moved reward")
+
+        if (new_state["location"] == self.prior_game_stats["location"] and self._read_m(0xD057) == 0):
+            self.no_move += 1
+            reward -= 1
+        if new_state["location"]["map"] == "PALLET_TOWN,":
+            if (new_state["location"]["y"]) < (self.prior_game_stats["location"]["y"]): #to attempt to go up
+                reward += 4 #<-Change
+                #print("Heading entrance of ROUTE_1")
+            elif (new_state["location"]["y"]) > (self.prior_game_stats["location"]["y"]):
+                reward -= 2
+                #print("Away from entrance of ROUTE_1")
+            elif 0 <= (new_state["location"]["y"]) < 3 and  (9 <new_state["location"]["y"] < 13):
+                reward += 4.5
+            #print("Near entrance of ROUTE_1")
+
+        return reward                  
     
     def _location_reward(self, new_state: dict[str, any]) -> float :
         reward = 0
-        if self.in_battle:
-            return 0
-        else:
-                
-            if new_state["location"]["map"] not in self.prev_loc:
-                self.prev_loc.add(new_state["location"]["map"])
-                self.different_loc = False
-                reward += 400
-            elif new_state["location"]["map"] != self.prior_game_stats["location"]["map"] and self.different_loc == True:
-                self.different_loc = False #After going into new location that is already visited, cannot come and get this reward until 200 steps has passed
-                reward += 100
-            elif new_state["location"]["map"] == self.prior_game_stats["location"]["map"]:
-                reward += 50 - min(self.same_loc, 50)            #may need to change this from += to -= and - to +
-                self.same_loc += 1                  
-                #after being stuck for 200 ticks
-                if self.same_loc == 2:
-                    self.different_loc = True
-                    reward -= 1
-                    self.same_loc = 0
-            else:
-                reward =0
-            return reward
-    
+
+        #New area 
+        if new_state["location"]["map"] not in self.prev_loc:
+            self.prev_loc.add(new_state["location"]["map"])
+            reward = 50
+            if new_state["location"]["map"] == "PALLET_TOWN,":
+                reward += 2
+            elif new_state["location"]["map"] == "ROUTE_1,":
+                reward += 3
+            elif new_state["location"]["map"] == "VIRIDEAN_CITY,":
+                reward += 4
+            elif new_state["location"]["map"] == "REDS_HOUSE_1F" or new_state["location"]["map"] == "REDS_HOUSE_2F" or new_state["location"]["map"] == "BLUES_HOUSE":
+                reward -= 1
+           #print("Newly visited location")
+        # elif new_state["location"]["map"] in self.prev_loc:
+        #     reward = 10
+            #print("Different location from previously")
+        
+        # if new_state["location"]["map"] == "OAKS_LAB,":
+        #     reward -= 5
+
+        if new_state["location"]["map"] == "OAKS_LAB," or new_state["location"]["map"] == "REDS_HOUSE_1F" or new_state["location"]["map"] == "REDS_HOUSE_2F" or new_state["location"]["map"] == "BLUES_HOUSE":
+            reward -= 1
+
+        return reward
     
     def _in_battle_reward(self) -> float :
         reward = 0
@@ -136,26 +167,23 @@ class PokemonBrock(PokemonEnvironment):
             enemy_max_hp = self._read_hp(0xCFF4)  #self._read_m(0xCFF4) | self._read_m(0xCFF5)
             turn_num = self._read_m(0xCCD5) #turn number starts from 0
 
-            if ((enemy_max_hp/2) < enemy_curr_hp < (enemy_max_hp)) and turn_num != self.prev_turn:
-                reward += 400
+            if ((enemy_max_hp/2) < enemy_curr_hp < (enemy_max_hp)) and turn_num > 0:
+                reward = 4
+                #print("Has dealt damage")
                 self.prev_turn = turn_num
-            elif (0 < enemy_curr_hp < (enemy_max_hp/2)) and turn_num != self.prev_turn:
-                reward += 200
+            elif (0 < enemy_curr_hp < (enemy_max_hp/2)) and turn_num > 0:
+                reward = 5
+                #print("Damaged below half")
                 self.prev_turn = turn_num
-            elif (enemy_curr_hp == 0) :
-                reward += 500
-                self.in_battle = False
             elif turn_num == self.prev_turn:
-                reward -= 0 + min(self.turn_unreward, 1)
-                #when it is continously stuck on the same turn
-                if turn_num < self.prev_turn:
-                    self.turn_unreward += 0.01
-                if self.turn_unreward == 2:
-                    self.turn_unreward = 0
+                #print("stuck in same turn")
+                reward = -1
                 self.prev_turn = turn_num
-            self.in_battle = True
+            else:
+
+                reward = 0
         else:
-            self.in_battle = False
+            #print("Not in batte")
             reward = 0
 
         return reward
@@ -193,15 +221,17 @@ class PokemonBrock(PokemonEnvironment):
         seen_reward = self._seen_reward(new_state)
         battle_reward = self._in_battle_reward()
         xp_reward = self._xp_reward(new_state)
+        #print(f"Location: {location_reward}, Movement: {movement_reward}, Seen: {seen_reward}, battle: {battle_reward}, xp: {xp_reward}")
 
         caught_reward = self._caught_reward(new_state)
 
-        total_reward = (movement_reward * 0.5
-                        + location_reward * 0.8
-                        + seen_reward *0.4
-                        + caught_reward *0.75
-                        + battle_reward * 0.9
-                        + xp_reward*0.5)
+        #Try multiplying all by 10?
+        total_reward = (movement_reward #* 0.4
+                        + location_reward #* 0.8
+                        + seen_reward #*0.4
+                        + caught_reward #*0.75
+                        + battle_reward #* 0.9  
+                        + xp_reward )#*0.5)
 
         return total_reward
 
@@ -212,24 +242,11 @@ class PokemonBrock(PokemonEnvironment):
     def _check_if_truncated(self, game_stats: dict) -> bool:
         # Implement your truncation check logic here
         if self.steps >= 1000:
-            self.same_loc = 0
-            self.turn_unreward = 0 
-            self.y_reward = 0
-            self.prev_turn = -1
-            self.prev_loc = {""}
-            self.in_battle = True
-            self.different_loc = False
-            self.complete_stuck = 0
             return 1
-        elif self.complete_stuck >= 100:
-            self.same_loc = 0
-            self.turn_unreward = 0 
-            self.y_reward = 0
-            self.prev_turn = -1
-            self.prev_loc = {""}
-            self.in_battle = True
-            self.different_loc = False
-            self.complete_stuck = 0
-            return 1
+        # elif self.no_move >= 500:
+        #     return 1
         else:
             return 0
+        
+        # Maybe if we run out of pokeballs...? or a max step count
+        #return self.steps >= 1000
